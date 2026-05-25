75c16117163dad99fb9123ea3e1414636733d7dc
m0j0man1ac <113955916+m0j0man1ac@users.noreply.github.com>
2026-05-26 01:21:04 +1000

    Add: trapping-rain-water - submission-3

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-3.py
@@ -0,0 +1,51 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:        
+        totalV = 0
+
+        i = 0
+        while(i<len(height)-1):
+            l = height[i]
+
+            if l <= 0:
+                i += 1
+                continue
+
+            j = i+1
+
+            noMatch = False
+            highestMatch = (-1,-1)
+            heightSum = 0
+
+            while j < len(height):
+                r = height[j]
+                if highestMatch[1] < r:
+                    highestMatch = (j, r)
+                
+                if l > r:
+                    heightSum += r
+                    j += 1
+                    if j == len(height):
+                        noMatch = True
+                    continue
+                
+                vol = min(l, r) * (j-i-1) - heightSum
+                print(f"testing pair - idx {i}:{j} h {height[i]}:{height[j]} - vol {vol} (-{heightSum})")
+                totalV += vol
+                break
+
+            if noMatch:
+                highI, highN = highestMatch
+                if not highN <= 0:
+                    sumHeights = 0
+                    for x in range(i+1, highI):
+                        sumHeights += height[x]
+                    vol = min(height[i], height[highI]) * (highI-i-1) - sumHeights
+                    totalV += vol
+                    
+                    i = highI
+                else:
+                    i+=1
+            else:
+                i = j
+            
+        return totalV
