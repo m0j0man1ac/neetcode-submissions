52aa656a8b3adcd476b8a7f7189a3954f0df64c8
m0j0man1ac <113955916+m0j0man1ac@users.noreply.github.com>
2026-05-26 21:48:34 +1000

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        openBDict = {'}':'{', ']':'[', ')':'('}
+
+        for c in s:
+            if c in ['{', '[', '(']: #open bracket, add to stack
+                stack.append(c)
+                continue
+
+            if stack:
+                if stack[-1] == openBDict[c]:
+                    stack.pop()
+                    continue
+
+            return False
+
+        return len(stack) == 0
