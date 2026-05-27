152714b603e99c5982dbb973b9a7bd69ce88d58c
m0j0man1ac <113955916+m0j0man1ac@users.noreply.github.com>
2026-05-28 01:28:26 +1000

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        t = temperatures
+        size = len(t)
+
+        stack = []
+        result = [0] * size
+
+        for i in range(size):
+            #pop case
+            while(True):
+                if len(stack)>0 and t[stack[-1]] < t[i]: #found a warmer day
+                    result[stack[-1]] = i - stack[-1]
+                    stack.pop()
+                else:
+                    break
+
+            stack.append(i)    
+
+        #things left in stack
+        for i in stack:
+            result[i] = 0
+
+        return result
+
