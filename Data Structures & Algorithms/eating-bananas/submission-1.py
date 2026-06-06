a893e62877fb845a7ae3137e0c4720859ef2b23b
m0j0man1ac <113955916+m0j0man1ac@users.noreply.github.com>
2026-06-07 00:50:29 +1000

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.py
@@ -0,0 +1,38 @@
+class Solution:
+    import math
+
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        size = len(piles)
+        
+
+        lPile = max(piles)
+        
+        print(f"largest pile {lPile}")
+        lastK = lPile
+
+        l,r = 1, lPile
+
+        while l<=r:
+            mid = l + (r-l)//2
+            print(f"mid {mid}")
+
+            sumC = 0
+
+            for i,n in enumerate(piles):
+                sumC += (n+mid-1)//mid
+
+            print(f"sumC {sumC}")
+
+            if sumC <= h:
+                lastK = min(mid, lastK)
+
+            #not fast enough, need faster eat speed
+            if sumC > h:
+                l = mid + 1
+            #potentially more time, can have slower speed, smaller mid
+            else:
+                r = mid - 1
+            
+
+
+        return lastK
