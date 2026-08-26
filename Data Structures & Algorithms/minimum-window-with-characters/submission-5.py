3afd336fd70be814259c5a3f2b35758f6c21f68a
Ryan <113955916+ryan-n-crowe@users.noreply.github.com>
2026-08-26 22:46:34 +1000

    Add: minimum-window-with-characters - submission-5

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-5.py
@@ -0,0 +1,51 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(t) == 0 or len(s)<len(t):
+            return ""
+
+        tMap : dict[str, int] = {}
+        matches = 0
+
+        for c in t:
+            tMap[c] = tMap.get(c, 0) + 1
+
+        sMap : dict[str, int] = {}
+
+        found = False
+        shortL = len(s)
+        shortIdx : tuple[int, int] = (0,0)
+
+        l,r = 0,0
+        while r < len(s):
+            rc = s[r]
+            if rc in tMap:
+                sMap[rc] = sMap.get(rc, 0) + 1
+                if tMap[rc] == sMap[rc]:
+                    matches += 1
+
+            #matches found
+            #try update answer
+            #try trim from left
+            while matches >= len(tMap) and l<=r:
+                found = True
+                if shortL > r-l:
+                    shortL = r-l
+                    shortIdx = (l,r)
+                
+                #if l<r:
+                lc = s[l]
+                if lc in tMap:
+                    sMap[lc] = sMap.get(lc, 0) - 1
+                    if sMap[lc] == tMap[lc] - 1:
+                        matches -= 1
+                
+                l += 1
+
+            r += 1
+
+        if found:
+            return s[shortIdx[0] : shortIdx[1]+1]
+
+        return ""
+
+
